33f336e37a3aaf3d29c242fcddccf28f811e35d2
Gean-Pierre Fernandez Perez <GEA2141067@maricopa.edu>
2020-10-26 20:12:28 -0700

    created python test, testing with simple math.

---
+++ Program/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte x = 0b_1000; // 2^n
+
+            int b = x >> 5;
+            Console.WriteLine($"{Convert.ToString(b, toBase: 2)}");
+        }
+    }
+}
