eddbad59059ed6937fcf23dea9877d6e11a0303a
Gea2141067 <73321431+Gea2141067@users.noreply.github.com>
2020-12-10 20:15:48 -0700

    I have add what is the boolean identity assigments including PartA and PartB using the  Logic Circuit code as reference for this assigment.

---
+++ Week3-BoolenIdentity/PartB/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is the table
+            var inputs = new List<IdentityInput>();
+            inputs.Add(new IdentityInput() { A = false, D = false});
+            inputs.Add(new IdentityInput() { A = false, D = true});
+            inputs.Add(new IdentityInput() { A = true, D = false});
+            inputs.Add(new IdentityInput() { A = true, D = true});
+
+            Console.WriteLine($"      Y           X           ~(x+y) = ~x.~y");
+            foreach (var item in inputs)
+            {
+                var firstIdentity = new FirstIdentity();
+                firstIdentity.SetInputD = item.D;
+                firstIdentity.SetInputA = item.A;
+                var secondIdentity = new SecondIdentity();
+                secondIdentity.SetInputD = item.D;
+                secondIdentity.SetInputA = item.A;
+                var firstoutput = firstIdentity.Validate();
+                var secondoutput = secondIdentity.Validate();
+                Console.WriteLine($" D = {firstIdentity.SetInputD}," +
+                    $" A = {firstIdentity.SetInputA}, " +
+                    $" Output= {firstoutput} = {secondoutput}");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine($"      Y           X           ~(x.y) = ~x+~y");
+            foreach (var item in inputs)
+            {
+                var thirdIdentity = new ThirdIdentity();
+                thirdIdentity.SetInputD = item.D;
+                thirdIdentity.SetInputA = item.A;
+                var fourthIdentity = new FourthIdentity();
+                fourthIdentity.SetInputD = item.D;
+                fourthIdentity.SetInputA = item.A;
+                var thirdoutput = thirdIdentity.Validate();
+                var fourthoutput = fourthIdentity.Validate();
+                Console.WriteLine($" D = {thirdIdentity.SetInputD}," +
+                     $" A = {thirdIdentity.SetInputA}, " +
+                     $" Output= {thirdoutput} = {fourthoutput}");
+            }
+                Console.ReadKey();
+        }
+    }
+}
