ea8f6ea27a24b5989c134318e7a0f293556db2b1
Gea2141067 <73321431+Gea2141067@users.noreply.github.com>
2020-12-11 09:28:15 -0700

    I have add the boolean game, i still need to implement the sounds, and some other options, like the negation.

---
+++ Week4-CompoundBooleanExpressions/BooleanGame/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace BooleanGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(157, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gold;
+            this.button2.Location = new System.Drawing.Point(157, 357);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "true";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Tomato;
+            this.button3.Location = new System.Drawing.Point(208, 357);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(44, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "false";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(51, 172);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Sound";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(135, 172);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(52, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Timer";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(208, 172);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(77, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Compound";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(300, 172);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(73, 17);
+            this.checkBox4.TabIndex = 6;
+            this.checkBox4.Text = "Challange";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft PhagsPa", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(85, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "True-False: A Boolean Game";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(394, 105);
+            this.panel1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(89, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 33);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "CSA Games V1.1";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.button4.FlatAppearance.BorderSize = 2;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(135, 195);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 48);
+            this.button4.TabIndex = 9;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.MouseHover += new System.EventHandler(this.button4_MouseHover);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.ForeColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(135, 249);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(138, 102);
+            this.panel2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 1;
+            this.label3.TextChanged += new System.EventHandler(this.label3_TextChanged);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(51, 14);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(32, 38);
+            this.button5.TabIndex = 0;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.TextChanged += new System.EventHandler(this.button5_TextChanged);
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(418, 533);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseHover += new System.EventHandler(this.button4_MouseHover);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Week4-CompoundBooleanExpressions/BooleanGame/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace BooleanGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int score = 0;
+        public static Random rnd = new Random();
+        public static String colors = "";
+        public static int number = 0;
+        public static string gateType = "";
+        public void Start()
+        {
+            button1.Text = "Restart";
+            button2.Enabled = true;
+            button2.BackColor = Color.Gold;
+            button3.Enabled = true;
+            button3.BackColor = Color.Tomato;
+            int randomnum = rnd.Next(1, 3);
+            button5.Text = randomnum.ToString();
+            Color[] c = { Color.Red, Color.Green, Color.Gray, Color.Yellow };
+            String[] names = { "Red", "Green", "Gray", "Yellow" };
+            String[] gate = { "|", "&&" };
+            button5.BackColor = c[rnd.Next(0, c.Length)];
+            button5.ForeColor = button5.BackColor == c[3] ? Color.Black : Color.White;
+            gateType = gate[rnd.Next(0, gate.Length)];
+            colors = names[rnd.Next(0, names.Length)];
+            number = randomnum;
+            label3.Text = ("(" + colors + " " + gateType + gateType + " " + number + ")");
+        }
+        public void ReStart()
+        {
+            score = 0;
+            button4.Text = "Score = " + score;
+            Start();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReStart();
+        }
+
+        private void button4_MouseHover(object sender, EventArgs e)
+        {
+        }
+
+        private void button5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (gateType == "|")
+            {
+                if(button5.BackColor == Color.FromName(colors)||
+                    button5.Text == number.ToString())
+                {
+                    score = score + 1;
+                    button4.Text = "Score = " + score;
+                    Start();
+
+                }
+                else
+                {
+                    button3.Enabled = false;
+                    button3.BackColor = Color.LightCoral;
+                    button2.Enabled = false;
+                    button2.BackColor = Color.Khaki;
+                }
+            }
+            else if(gateType == "&&")
+            {
+                if(button5.BackColor == Color.FromName(colors) &&
+                    button5.Text == number.ToString())
+                {
+                    score = score + 1;
+                    button4.Text = "Score = " + score;
+                    Start();
+                }
+                else
+                {
+
+                    button3.Enabled = false;
+                    button3.BackColor = Color.LightCoral;
+                    button2.Enabled = false;
+                    button2.BackColor = Color.Khaki;
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (gateType == "|")
+            {
+                if (button5.BackColor != Color.FromName(colors) &&
+                    button5.Text != number.ToString()) 
+                {
+                    score = score + 1;
+                    button4.Text = "Score = " + score;
+                    Start();
+                }
+                else
+                {
+                    button3.Enabled = false;
+                    button3.BackColor = Color.LightCoral;
+                    button2.Enabled = false;
+                    button2.BackColor = Color.Khaki;
+
+                }
+            }
+            else if (gateType == "&&")
+            {
+                if (button5.BackColor != Color.FromName(colors) ||
+                    button5.Text != number.ToString())
+                {
+                    score = score + 1;
+                    button4.Text = "Score = " + score;
+                    Start();
+                }
+                else
+                {
+                    button3.Enabled = false;
+                    button3.BackColor = Color.LightCoral;
+                    button2.Enabled = false;
+                    button2.BackColor = Color.Khaki;
+                }
+            }
+        }
+    }
+}
